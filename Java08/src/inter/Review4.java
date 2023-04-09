214d9cb32096a46b7924c31cf913f084c08993f0
baekmir0505 <bmrbag0601@naver.com>
2023-04-09 11:43:09 +0900

    2023-04-09 Java day08

---
+++ Java08/src/inter/Review4.java
@@ -0,0 +1,28 @@
+package inter;
+
+public class Review4 implements Review3{
+	// 인터페이스를 받으면 클래스명에 빨간 밑줄이 생성됨
+	// 클래스명 밑의 빨간 밑줄에 마우스를 갖다대고
+	// 첫번째 항목을 클릭
+	
+	@Override
+	public void test1() {
+		System.out.println(str);
+	}
+
+	@Override
+	public void test2() {
+		System.out.println("재정의");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(str2);       // static은 바로 쓸 수 있음
+		
+		Review4 review4 = new Review4();
+		review4.test1();
+		review4.test2();
+	}
+}
+
+// extends 상속 : 자바는 한 클래스에 하나만 가능
+// implements : 여러개 가능
---
+++ Java08/src/polymorphsim/Pet.java
@@ -0,0 +1,6 @@
+package polymorphsim;
+
+public interface Pet {
+	public void 울기();
+	public void 먹기();
+}
