dd1b6e1fabd2b810f43dbca6f24849d818aa385f
baekmir0505 <bmrbag0601@naver.com>
2023-05-15 22:13:19 +0900

    2023-05-15 Java 16

---
+++ Java16/src/review/_07_Extends.java
@@ -0,0 +1,30 @@
+package review;
+
+public class _07_Extends {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Extends{
+	
+}
+
+class MyClass7{
+	// 클래스 : 변수+함수
+	private String str;
+	
+	// 생성자 (클래스의 이름과 동일한 이름의 메서드, 객체화할 때 자동 호출)
+	public MyClass7() {
+		
+	}
+	
+	// 메서드는 ()를 갖고있다
+	public void setStr(String str) {
+		
+	}
+	
+	public String getStr() {
+		return str;
+	}
+}
